42a80710ca64bb3ee38e5b138394801efb66cdbd
ABHISHEK KUMAR 17BIT0371 <abhishek.kumar2017@vitstudent.ac.in>
2020-07-20 22:09:14 +0530

    Commit

---
+++ ProductListMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProductListMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ProductListMVC/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductListMVC.Models
+{
+    public class Product
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string QualityParameter { get; set; }
+
+        [Required]
+        public string ProcessParameter { get; set; }
+
+        [Required]
+        public string Equipment { get; set; }
+
+
+    }
+}
